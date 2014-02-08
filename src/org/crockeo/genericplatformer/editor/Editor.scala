765a637b2c00e27f3ef059c1fafee1882a6bb66e
Crockeo <crockeo@gmail.com>
2014-02-08 01:35:57 -0500

    Started to implement the editor.

--- src/org/crockeo/genericplatformer/editor/Editor.scala
+++ src/org/crockeo/genericplatformer/editor/Editor.scala
@@ -1,16 +1,36 @@
 package org.crockeo.genericplatformer.editor
 
-import com.badlogic.gdx.ApplicationListener
+import com.badlogic.gdx.graphics.{ OrthographicCamera, GL10 }
+import com.badlogic.gdx.{ ApplicationListener, Gdx }
 
-import org.crockeo.genericplatformer.Config
+import org.crockeo.genericplatformer.game.World
+import org.crockeo.genericplatformer.{ InputConstructor, Graphics, Config }
 
 class Editor(cfg: Config) extends ApplicationListener {
-  // TODO: Implement editor
-  def create {}
+  var cam: OrthographicCamera = null
+  var world: World = null
+  
+  // Initializing the game
+  def create {
+    cam = new OrthographicCamera(Gdx.graphics.getWidth, Gdx.graphics.getHeight)
+    cam.setToOrtho(true, Gdx.graphics.getWidth, Gdx.graphics.getHeight)
+    
+    Graphics.create(cam)
+    world = World.empty
+  }
+  
+  // Cleaning up
   def dispose {}
   
-  def render {}
+  // Updating and rendering
+  def render {
+    val input = InputConstructor(cfg.inputConfig)
+    val gl = Gdx.gl10
+    
+    // TODO: Finish the editor
+  }
   
+  // UNUSED \\
   def pause {}
   def resume {}
   
